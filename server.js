049505617088d9d777714862d26b01ac0f39ce95
MacBook <admin@MacBooks-MacBook-Air-2.local>
2020-03-08 23:37:33 +0530

    Created the MongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
+//connect Database
+connectDB();
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
