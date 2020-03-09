aac8c38d8239279f31f8c64ef93fe09a4722b78f
MacBook <admin@MacBooks-MacBook-Air-2.local>
2020-03-09 10:22:49 +0530

    Added mongoDB user save option & hashed password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/Users");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
 
 //@route POST api/users
 //@desc Register user route
@@ -17,12 +20,44 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.status(200).json({ message: "User registered sucesfully" });
+    //deconstruct user object
+    const { name, email, password } = req.body;
+    try {
+      //check if the user exsist
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: "User Already Exsist" }] });
+      }
+      //create the avatar using gravitar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      //create the user instance
+      user = new User({
+        name,
+        email,
+        password,
+        avatar
+      });
+      //hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      //save user to DB
+      await user.save();
+      res.status(200).json({ message: "User registered sucesfully" });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ errors: "Server Error" });
+    }
     console.log(req.body);
   }
 );
